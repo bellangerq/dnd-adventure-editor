5e060f6d9cb947d3c509fc2e22aa8b6f548f284c
Quentin Bellanger <hello@quentin-bellanger.com>
2021-10-07 18:12:01 +0200

    add lang and title elements

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,17 @@
+import Head from 'next/head'
+
 import '../styles/globals.css'
 import '../styles/print.css'
 
 function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <title>DnD Adventure Editor</title>
+      </Head>
+      <Component {...pageProps} />
+    </>
+  )
 }
 
 export default App
